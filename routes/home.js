f5cca680d92ef3cdc150fc969259f0cef15b1322
mj <1@1.com>
2018-05-12 19:52:05 +0800

    4

--- routes/home.js
+++ routes/home.js
@@ -2,9 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-    var playerName = req.session.playerName;
+    //var playerName = req.session.playerName;
 
-    res.render('home', {playerName : playerName});
+    res.render('home', {playerName : "213"});
 });
 
 module.exports = router;
