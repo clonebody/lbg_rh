1a767a6653ba27cc957e38f2cabb3f99dd4424db
mj <1@1.com>
2018-05-19 17:05:06 +0800

    console 4

--- server.js
+++ server.js
@@ -106,7 +106,6 @@ if (app.get('env') != "development") {
           },function (err) {
             console.log("err");
             console.log(err);
-            })
           })
       })
     };
