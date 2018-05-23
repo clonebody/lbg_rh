2ca9332ef88fdbbb38ed19552b805b97bef0da37
mj <1@1.com>
2018-05-23 18:12:54 +0800

    log 9

--- routes/account.js
+++ routes/account.js
@@ -64,7 +64,7 @@ router.post('/action', function(req, res, next) {
             break;
         case "login":
             opr.findAccount(account).then((list) => {
-                if (list.length != 1) {
+                if (list.length != 0) {
                     var item = list[0];
                     if (item.password == password) {
                         success();
